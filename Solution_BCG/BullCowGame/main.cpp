0ea3271d389e95c2efe193825b433d6d4dd53d82
TWalk007 <walkertb03@gmail.com>
2018-04-30 16:31:14 -0400

    Finished the Do/While loop section.

--- Solution_BCG/BullCowGame/main.cpp
+++ Solution_BCG/BullCowGame/main.cpp
@@ -3,21 +3,54 @@
 
 using namespace std;
 
-int main()
-{	
-	// Introducing the game.
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+string Guess;
+bool AskToPlayAgain();
+
+
+int main() {		
+	
+	do {
+		PrintIntro();
+		PlayGame();	
+	} 
+	while (AskToPlayAgain());
+
+	return 0; //exit the application.
+}
+
+// Introducing the game.
+void PrintIntro() {
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
-	// get a guess from the player.
-	cout << "What is your guess? ";
-	string Guess = "";
-	cin >> Guess;
-
-	// repeat the guess back to them.
-	cout << "Your guess was: " << Guess << endl;
-
-	return 0;
+	return;
 }
+
+void PlayGame() {
+	// loop for the number of turns asking for guesses.
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+string GetGuess() {
+	cout << "Enter your guess? ";
+	getline(cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain() {
+	cout << "Do you want to play again (y/n) ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+
