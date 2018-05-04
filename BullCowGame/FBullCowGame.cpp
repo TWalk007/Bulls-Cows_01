8a3b4f1797dea44ba89371eac2f11d56c4536a4a
Tom Walker <walkertb03@gmail.com>
2018-05-04 09:58:17 -0400

    Update to Ben's Files

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -1,14 +1,16 @@
 #include "FBullCowGame.h"
 
+using int32 = int;
 
-FBullCowGame::FBullCowGame() { Reset();}
+FBullCowGame::FBullCowGame() { Reset(); }
 
 int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
 bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
 
-void FBullCowGame::Reset() {
+void FBullCowGame::Reset()
+{
 	constexpr int32 MAX_TRIES = 8;
 	const FString HIDDEN_WORD = "planet";
 
@@ -19,42 +21,57 @@ void FBullCowGame::Reset() {
 	return;
 }
 
-EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const { 
-	if (false) { // if the guess isn't an isogram
+
+
+
+EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
+{
+	if (false) // if the guess isn't an isogram
+	{
 		return EGuessStatus::Not_Isogram;
-	} else if (false) { // if the guess isn't all lowercase
+	}
+	else if (false) // if the guess isn't all lowercase
+	{
 		return EGuessStatus::Not_Lowercase;
-	} else if (Guess.length() != GetHiddenWordLength()) { // if the guess length is wrong
+	}
+	else if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	{
 		return EGuessStatus::Wrong_Length;
-	} else {
+	}
+	else
+	{
 		return EGuessStatus::OK;
-	}	
+	}
 }
 
-// receives a valid guess, increments turn, and returns count.
-FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess) {
+
+// receives a VALID guess, incriments turn, and returns count
+FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
+{
 	MyCurrentTry++;
 	FBullCowCount BullCowCount;
-	int32 WordLength = MyHiddenWord.length(); // assuming the word is the same length as guess.
+	int32 WordLength = MyHiddenWord.length(); // assuming same length as guess
 
-	// loop through all letters in the hidden word.
+	// loop through all letters in the hidden word
 	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++) {
-		// compare letters against the guess.
+		// compare letters against the guess
 		for (int32 GChar = 0; GChar < WordLength; GChar++) {
 			// if they match then
 			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
-				if (MHWChar == GChar) { //if the indices are the same (same location in the word)
-					BullCowCount.Bulls++; // increment bulls
+				if (MHWChar == GChar) { // if they're in the same place
+					BullCowCount.Bulls++; // incriment bulls
 				}
 				else {
-					BullCowCount.Cows++; // must be a cows
+					BullCowCount.Cows++; // must be a cow
 				}
 			}
 		}
 	}
 	if (BullCowCount.Bulls == WordLength) {
 		bGameIsWon = true;
-	} else {
+	}
+	else
+	{
 		bGameIsWon = false;
 	}
 	return BullCowCount;
--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -1,40 +1,44 @@
 #pragma once
 #include <string>
-#include <iostream>
 
 using FString = std::string;
 using int32 = int;
 
-// two integers initialized at zero.
-struct FBullCowCount {
+// all values intialised to zero
+struct FBullCowCount
+{
 	int32 Bulls = 0;
 	int32 Cows = 0;
 };
 
-enum class EGuessStatus {
+
+enum class EGuessStatus
+{
 	Invalid_Status,
 	OK,
 	Not_Isogram,
 	Wrong_Length,
-	Not_Lowercase	
+	Not_Lowercase
 };
 
-class FBullCowGame {
 
+class FBullCowGame
+{
 public:
-	FBullCowGame(); // this is the constructor.
+	FBullCowGame(); // constructor
 
-	int32 GetMaxTries() const;  // This is saying that the properties cannot change.
+	int32 GetMaxTries() const;
 	int32 GetCurrentTry() const;
 	int32 GetHiddenWordLength() const;
 	bool IsGameWon() const;
 	EGuessStatus CheckGuessValidity(FString) const;
 
-	void Reset();
+	void Reset(); // TODO make a more rich return value.
 	FBullCowCount SubmitValidGuess(FString);
-	
+
+// ^^ Please try and ignore this and focus on the interface above ^^
 private:
-	// see constructor for initialization.
+	// see constructor for initialisation
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
 	FString MyHiddenWord;
